2c047730ddb24be6660d5457ec37e655de8b81df
hareeshcs33 <hareeshcs33@gmail.com>
2020-07-11 12:19:51 +0530

    created separate css and javascript files.

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("digital clock!!!");
+
+function showTime(){
+
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let formats = convertFormat(hours);
+
+    hours = checkTime(hours);
+
+    hours = addZero(hours);
+    minutes = addZero(minutes);
+    seconds = addZero(seconds);
+
+    document.getElementById('clock').innerHTML = `${hours} : ${minutes} : ${seconds} ${formats}`;
+}
+function convertFormat(time){
+    let format = 'AM';
+    if(time >= 12){
+        format = 'PM'
+    }
+    return format;
+}
+function checkTime(time){
+    if(time > 12){
+        time = time - 12;
+    }
+    if(time === 0){
+        time = 12;
+    }
+    return time;
+}
+function addZero(time) {
+    if(time < 10) {
+        time = "0" + time;
+    }
+    return time;
+}
+
+setInterval(showTime, 1000);
